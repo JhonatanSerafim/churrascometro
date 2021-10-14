524cbd2438c0ef03105153d9be0189d0e836471a
Jhonatan Serafim <60790080+JhonatanSerafim@users.noreply.github.com>
2021-10-14 10:06:46 -0300

    :tada: Adicionado estilo básico

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # churrascometro
+
+##EM CONSTRUÇÃO
+
+###INSTRUÇÕES DO PROJETO 
+
+Carne                - 440gr por pessoa     +       de 6 horas = 650gr
+Cerveja              - 1200ml por pessoa    +       de 6 horas = 2000ml
+Refrigerante/agua    - 1000ml por pessoa    +       de 6 horas 1500ml
+
+Crianças valem por 0,5
---
+++ script.js
@@ -0,0 +1,7 @@
+/**************** INSTRUÇÕES DO PROJETO ***********************/
+
+// Carne                - 440gr por pessoa     +       de 6 horas = 650gr
+// Cerveja              - 1200ml por pessoa    +       de 6 horas = 2000ml
+// Refrigerante/agua    - 1000ml por pessoa    +       de 6 horas 1500ml
+
+// Crianças valem por 0,5
